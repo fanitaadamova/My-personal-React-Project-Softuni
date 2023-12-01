1b00dee734a7ce8aad999f5496bae3adab3b3da2
fanitaadamova <113979211+fanitaadamova@users.noreply.github.com>
2023-12-01 13:19:43 +0200

    Added commnet list and add comment functionality in details product page

--- client/src/components/features/products/details-of-product/ProductDetails.jsx
+++ client/src/components/features/products/details-of-product/ProductDetails.jsx
@@ -1,52 +1,65 @@
 import styles from './ProductDetails.module.css';
-import { useEffect, useState, useContext } from 'react';
+import { useEffect, useState, useContext, useReducer } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 
 import * as techniqueAPI from '../../../../api/techniqueAPI';
 import * as purchaseAPI from '../../../../api/purchaseAPI';
+import * as commentAPI from '../../../../api/commentAPI';
+import useForm from '../../../../hooks/useForm';
 import { AuthContext } from '../../../../contexts/AuthContext';
+import reducer from './commentReducer';
 
 import Loader from '../../../shared/Loader';
 import DeleteModal from '../delete-product/DeleteModal';
 
+const formInitialState = { comment: '', };
+
+
 export default function ProductDetails() {
     const navigate = useNavigate();
+    const [comments, dispatch] = useReducer(reducer, []);
 
     const { auth } = useContext(AuthContext);
     const { productId } = useParams();
     const [isLoading, setIsLoading] = useState(true);
     const [showDelete, setShowDelete] = useState(false);
     const [isBought, setIsBought] = useState(false);
-    const [productDetails, setProductDetails] = useState({});
     const [hasServerError, setHasServerError] = useState(false);
+    const [productDetails, setProductDetails] = useState({});
     const [serverError, setServerError] = useState({});
 
 
-
     useEffect(() => {
-
-        setIsLoading(true);
-
         techniqueAPI.getOne(productId)
-            .then(result => setProductDetails(result))
+            .then(result => {
+                setProductDetails(result);
+            })
             .catch(err => {
                 if (err.code == 404) { navigate('/not-found'); }
                 console.log(err.message);
             })
             .finally(() => setIsLoading(false));
 
-
         if (auth) {
             purchaseAPI.getBuyersOfProduct(productId)
                 .then(result => result.includes(auth._id) ? setIsBought(true) : setIsBought(false))
                 .catch(err => {
-                    console.log();
                     setHasServerError(true);
                     setServerError(err.message);
                     console.log(err.message);
                 })
                 .finally(() => setIsLoading(false));
         }
+
+        commentAPI.getAllComments(productId)
+            .then((result) => {
+
+                dispatch({
+                    type: 'GET_ALL_COMMENTS',
+                    payload: result,
+                });
+            });
+
 
     }, [productId, auth, navigate]);
 
@@ -68,8 +81,7 @@ export default function ProductDetails() {
     const buyClickHandler = (e) => {
         e.preventDefault();
 
-        const userId = auth._id;
-        purchaseAPI.purchase(productId, userId)
+        purchaseAPI.purchase(productId, auth._id)
             .then(() => setIsBought(true))
             .catch(err => console.log(err));
 
@@ -85,6 +97,26 @@ export default function ProductDetails() {
             isLogdin = true;
         }
     }
+
+
+    // ADD Comments
+    const addCommentHandler = async (values) => {
+        const newComment = await commentAPI.create(productId, values.comment);
+
+        const email = auth.email;
+        newComment.owner = { email };
+
+        dispatch({ 
+            type: 'ADD_COMMENT',
+             payload: newComment
+             });
+
+        values.comment = '';
+    };
+
+    const { values, onChange, onSubmit } = useForm(addCommentHandler, formInitialState);
+
+
 
     return (
         <div className={styles.product_info}>
@@ -141,10 +173,44 @@ export default function ProductDetails() {
                     )}
 
                 </div>
-
                 <br />
-
             </div>
+
+
+            {/* SECTION COMMENTS */}
+            <section className={styles.comments}>
+                <div className={styles.details_comments}>
+                    <h2>Коментари</h2>
+                    <ul>
+                        {comments.map(({ _id, text, owner: { email } }) => (
+                            <li key={_id} className={styles.comment_info}>
+                                <i className="fa fa-user-circle"></i>
+                                <span className={styles.email}> {email}</span>
+                                <p className={styles.text}>{text}</p>
+                            </li>
+                        ))}
+                    </ul>
+
+                    {comments.length === 0 && (
+                        <p className={styles.no_comment}>Няма коментари.</p>
+                    )}
+                </div>
+
+            </section>
+
+            {/* ADD Comments SECTION */}
+            {(isLogdin || isOwner) && (
+                <section className={styles.create_comment}>
+                    <label>Добави нов коментар</label>
+                    <form className={styles.form} onSubmit={onSubmit}  >
+                        <textarea name="comment"
+                            value={values.comment}
+                            onChange={onChange}
+                            placeholder="Коментар......"></textarea>
+                        <input disabled={values.comment == ''} className={styles.btn_submit} type="submit" value="Изпрати" />
+                    </form>
+                </section>
+            )}
 
         </div>
     );
--- client/src/components/features/users/profile/Profile.jsx
+++ client/src/components/features/users/profile/Profile.jsx
@@ -136,14 +136,22 @@ export default function Profile() {
 
                 <div className={styles.bought_products}>
                     <div className="container">
-                        <div className={styles.titlepage}>
-                            <h2>Покупки -  <span className={styles.sum}>{totalSum} BGN</span></h2>
-                        </div>
-                        <div className={styles.dummy}></div>
 
 
                         {boughtProduct.length > 0
                             ? (<>
+
+                                <div className={styles.titlepage}>
+                                    <h2>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
+                                            <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5M3.14 5l.5 2H5V5zM6 5v2h2V5zm3 0v2h2V5zm3 0v2h1.36l.5-2zm1.11 3H12v2h.61zM11 8H9v2h2zM8 8H6v2h2zM5 8H3.89l.5 2H5zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0m9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0" />
+                                        </svg>
+                                        <span className={styles.sum}> {totalSum} BGN </span>
+                                        - {boughtProduct.length} покупки</h2>
+
+                                </div>
+                                <div className={styles.dummy}></div>
+
                                 <div className="row">
                                     {boughtProduct.map(tech => (
                                         < Product
@@ -159,9 +167,18 @@ export default function Profile() {
 
                                 </div>
                             </>)
-                            : <div className={styles.no_technique}>
-                                <p className={styles.no_content}>Няма закупени продукти.</p>
-                            </div>
+                            :
+                            <>
+                                <div className={styles.titlepage}>
+                                    <h2>Няма закупени продукти</h2>
+                                    <div className={styles.dummy}></div>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="160" height="160" fill="currentColor" className="bi bi-cart-x" viewBox="0 0 16 16">
+                                        <path d="M7.354 5.646a.5.5 0 1 0-.708.708L7.793 7.5 6.646 8.646a.5.5 0 1 0 .708.708L8.5 8.207l1.146 1.147a.5.5 0 0 0 .708-.708L9.207 7.5l1.147-1.146a.5.5 0 0 0-.708-.708L8.5 6.793z" />
+                                        <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0m7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0" />
+                                    </svg>
+                                </div>
+
+                            </>
                         }
 
                     </div>
