fbfedb014b8aeab3238874b51b40794ceb742226
fanitaadamova <113979211+fanitaadamova@users.noreply.github.com>
2024-01-02 11:39:03 +0200

    Added Jest Unit Testing to React Vite + additional lybraries

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,5 +1,15 @@
 import App from './App';
+import { render , screen} from '@testing-library/react';
+
 
 test('Always true test', () => {
     expect(true).toBe.true;
+});
+
+test('Heading should be Tech Shop', () => {
+    render(<App />);
+
+   const headingElement =  screen.getByText('Tech Shop');
+
+   expect(headingElement).toBeInTheDocument();
 });
